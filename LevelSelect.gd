f046a4cbaa867e6b32b809b819104f16821a442c
AndrewOkpala <167389959+AndrewOkpala@users.noreply.github.com>
2025-04-12 21:38:23 -0700

    Assets Scene
    Made a scene containing different objects and walls

--- LevelSelect.gd
+++ LevelSelect.gd
@@ -4,3 +4,11 @@ extends Control
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Sandbox.tscn")
 	pass # Replace with function body.
+
+
+func _on_assets_button_pressed() -> void:
+	await self.ready
+	var tree = get_tree()
+	tree.change_scene_to_file("res://scenes/test.tscn")
+	#await(get_tree().change_scene_to_file("res://scenes/test.tscn"))
+	pass # Replace with function body.
