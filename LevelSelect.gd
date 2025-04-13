fd584fb8858ba1328d6fb9df92f162713228f859
AndrewOkpala <167389959+AndrewOkpala@users.noreply.github.com>
2025-04-13 15:42:28 -0700

    title

--- LevelSelect.gd
+++ LevelSelect.gd
@@ -9,3 +9,8 @@ func _on_button_pressed() -> void:
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Object_Enemy_test.tscn")
 	pass # Replace with function body.
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Walls.tscn")
+	pass # Replace with function body.
